e9bc1656c7b096120d327aaad9739201b024cab7
monthwolf <52775320+monthwolf@users.noreply.github.com>
2024-07-17 15:11:22 +0800

    Update fix.ts

--- fix.ts
+++ fix.ts
@@ -1,14 +1,27 @@
-const defaultSteamIconsPath = "C:/Program Files (x86)/Steam/steam/games";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  boolean: ["help", "cdn"],
+  string: ["p1",'p2'],
+  default: { p2: "C:/Program Files (x86)/Steam/steam/games",p1: "."},
+});
+if (flags.help) {
+  console.log(`Usage: deno run --allow-read --allow-write fix.ts [--p1 <path>] [--p2 <path>] [--cdn] [--color] [--help]`);
+  console.log(`修复目录下的空白steam图标`);
+  console.log(`--p1 <path>    需要搜索的目录, 默认为当前目录".".`);
+  console.log(`--p2 <path>    steam图标目录, 默认为"C:/Program Files (x86)/Steam/steam/games".`);
+  console.log(`--cdn          是否使用cloudflare CDN，默认为False.`);
+  console.log(`--help         显示帮助信息.`);
+  Deno.exit(0)
+}
 const CDN1 = "akamai";
 const CDN2 = "cloudflare";
-const useCDN = Deno.args[2] == 0 ? CDN1 : CDN2;
-const steamIconsPath = Deno.args[1] || defaultSteamIconsPath;
-const searchPath = String(Deno.args[0] || ".");
+const useCDN = flags.cdn? CDN2 : CDN1;
+console.log(`使用CDN: ${useCDN}`);
+console.log(`Steam图标目录: "${flags.p2}"`);
+console.log(`待修复图标的目录: "${flags.p1}"\n`);
 
-console.log(`Steam icons path: "${steamIconsPath}"`);
-console.log(`Searching shortcuts in: "${searchPath}"\n`);
-
-for await (const entry of Deno.readDir(searchPath)) {
+for await (const entry of Deno.readDir(flags.p1)) {
   if (!entry.isFile || !entry.name.endsWith(".url")) {
     continue;
   }
@@ -22,12 +35,12 @@ for await (const entry of Deno.readDir(searchPath)) {
   const localIconName = linkContent.match(/(\w|\d)+.ico/m)?.[0];
   const iconUrl = `https://cdn.${useCDN}.steamstatic.com/steamcommunity/public/images/apps/${gameId}/${localIconName}`;
   const hasIconFile = localIconName &&
-    await Deno.stat(steamIconsPath + "/" + localIconName).catch(() => {});
-  const status =  hasIconFile ? "ok" : "missed";
+    await Deno.stat(flags.p2 + "/" + localIconName).catch(() => {});
+  const status =  hasIconFile ? "完好" : "缺失";
   if (!hasIconFile && iconUrl) {
     const iconBuffer = await fetch(iconUrl).then((res) => res.arrayBuffer());
     await Deno.writeFile(
-      steamIconsPath + "/" + localIconName,
+      flags.p2 + "/" + localIconName,
       new Uint8Array(iconBuffer),
     );
   }
@@ -39,4 +52,4 @@ for await (const entry of Deno.readDir(searchPath)) {
   console.groupEnd();
 }
 
-console.log("Done!");
+console.log("完成!");
